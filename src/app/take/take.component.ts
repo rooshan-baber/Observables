bb620de052b83ac37983872662697a3292826d2f
rooshan baber <rooshanbaber07@gmail.com>
2023-10-06 12:10:44 +0500

    ads

--- src/app/take/take.component.ts
+++ src/app/take/take.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DesignUtilityService } from '../Services/design-utility.service';
+import { Subscription, from, interval, take, takeLast, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-take',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./take.component.css']
 })
 export class TakeComponent {
+  constructor(private service: DesignUtilityService) {}
+  source = interval(1000);
+  obs1! : Subscription;
+  obs2! : Subscription;
+  obs3! : Subscription;
+  Names = ['Rooshan', 'Baber', 'Ahmed', 'Ali', 'Aneeq', 'Basit','Fasih',"Haroon"];
+  ngOnInit(): void {
+     const nameSource = from(this.Names);
+    //Ex - 01
+    this.obs1 = nameSource
+    .pipe(take(4))
+    .subscribe(res=>{
+      this.service.print(res,'elContainer')
+    });
 
+    //Ex - 02
+    this.obs2 = nameSource
+    .pipe(takeLast(4))
+    .subscribe(res=>{
+      this.service.print(res,'elContainer2')
+    });
+
+    //Ex - 03
+    this.obs3 = this.source
+    .pipe(takeUntil(timer(6000)))
+    .subscribe(res=>{
+      this.service.print(res,'elContainer3')
+    });
+  }
 }
--- src/app/tap/tap.component.ts
+++ src/app/tap/tap.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { Subscription, interval, map, tap } from 'rxjs';
+import { DesignUtilityService } from '../Services/design-utility.service';
 
 @Component({
   selector: 'app-tap',
   templateUrl: './tap.component.html',
-  styleUrls: ['./tap.component.css']
+  styleUrls: ['./tap.component.css'],
 })
 export class TapComponent {
+  constructor(private service: DesignUtilityService) {}
+  
+  Arr = ['Rooshan', 'Baber', 'Ahmed', 'Ali', 'Aneeq', 'Basit','Fasih',"Haroon"];
+  Colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Pink', 'Orange', 'Brown'];
+  mycolor: any;
+  source = interval(1000);
+  ngOnInit(): void {
+    //Ex - 01
+    let obsSub: Subscription;
+    obsSub = this.source
+    .pipe(
+      tap(res =>{
+        if(res >= this.Arr.length){
+          obsSub.unsubscribe();
+        }
+      })
+      ,map(data => 
+      this.Arr[data]
+    ))
+    .subscribe((res) => {
+      this.service.print(res, 'elContainer');
+    });
 
-}
+      //Ex - 02
+      let obsSub2: Subscription;
+      obsSub2 = this.source
+      .pipe(
+        tap(res =>{
+          if(res >= this.Colors.length){
+            obsSub2.unsubscribe();
+          }
+          this.mycolor = this.Colors[res];
+        })
+        ,map(data => 
+        this.Colors[data]
+      ))
+      .subscribe((res) => {
+        this.service.print(res, 'elContainer2');
+      });
+    }
+  }
